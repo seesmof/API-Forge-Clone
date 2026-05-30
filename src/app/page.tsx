bf5e3011f44828f6664cc236fce3be30fa0441f6
Oleg Onyshchenko <seesmwork@gmail.com>
2026-05-30 18:22:46 +0300

    feat: add the NextJS scaffolding, start working

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Header from "@/components/Header";
+
+export default function Page() {
+  return (
+    <div className="flex flex-col">
+      <Header />
+    </div>
+  );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,3 @@
+export default function Header() {
+  return <header className="bg-white border-b border-slate-400 "></header>;
+}
