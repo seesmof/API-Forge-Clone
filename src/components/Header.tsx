54f48ff1a6723c1d5b52126e2f7ecae0515b4447
Oleg Onyshchenko <seesmwork@gmail.com>
2026-05-30 18:40:35 +0300

    feat: add the menu to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,64 @@
-import { Menu } from "@deemlol/next-icons";
+"use client";
+
+import { Menu, X } from "@deemlol/next-icons";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
+
+const MenuItemClasses = "hover:text-stone-700 transition-all duration-150";
 
 export default function Header() {
-  return (
-    <header className="bg-white/90 border-b border-stone-200">
-      <div className="flex justify-between mx-auto px-4 py-2 items-center">
-        <Link href={"/"}>
-          <Image
-            src={"/logo.svg"}
-            width={120}
-            height={120}
-            alt="A logo image of API Forge."
-            className="h-8"
-          />
-        </Link>
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
 
-        <div className="flex flex-row gap-3">
-          <button className="bg-orange-600 text-white font-semibold text-sm px-4 py-2 rounded-md hover:bg-orange-700 duration-150 transition-all cursor-pointer">
-            <Link href={"/pricing"}>Get API Key</Link>
-          </button>
-          <button className="hover:bg-stone-200 rounded-md p-1">
-            <Menu />
-          </button>
+  return (
+    <div className="top-0 sticky">
+      <header className="bg-white/90 border-b border-stone-200">
+        <div className="flex justify-between mx-auto px-4 py-2 items-center">
+          <Link href={"/"}>
+            <Image
+              src={"/logo.svg"}
+              width={120}
+              height={120}
+              alt="A logo image of API Forge."
+              className="h-8"
+            />
+          </Link>
+
+          <div className="flex flex-row gap-3">
+            <button className="bg-orange-600 text-white font-semibold text-sm px-4 py-2 rounded-md hover:bg-orange-700 duration-150 transition-all cursor-pointer">
+              <Link href={"/pricing"}>Get API Key</Link>
+            </button>
+            <button
+              className="hover:bg-stone-200 rounded-md p-1"
+              onClick={() => setIsMenuOpen((isMenuOpen) => !isMenuOpen)}
+            >
+              {isMenuOpen ? <X /> : <Menu />}
+            </button>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+
+      {isMenuOpen && (
+        <div className="bg-white border-b border-stone-200">
+          <div className="font-semibold flex-col flex p-3 gap-5">
+            <Link className={MenuItemClasses} href={"/services"}>
+              Products
+            </Link>
+            <Link className={MenuItemClasses} href={"/pricing"}>
+              Pricing
+            </Link>
+            <Link className={MenuItemClasses} href={"/about"}>
+              About
+            </Link>
+            <Link className={MenuItemClasses} href={"/blog"}>
+              Blog
+            </Link>
+            <Link className={MenuItemClasses} href={"/contact"}>
+              Contact
+            </Link>
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
