eda17e0e3f0941d8326385780bf4c78cbde4c761
Oleg Onyshchenko <seesmwork@gmail.com>
2026-05-30 18:33:21 +0300

    feat: add the header markup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,30 @@
+import { Menu } from "@deemlol/next-icons";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Header() {
-  return <header className="bg-white border-b border-slate-400 "></header>;
+  return (
+    <header className="bg-white/90 border-b border-stone-200">
+      <div className="flex justify-between mx-auto px-4 py-2 items-center">
+        <Link href={"/"}>
+          <Image
+            src={"/logo.svg"}
+            width={120}
+            height={120}
+            alt="A logo image of API Forge."
+            className="h-8"
+          />
+        </Link>
+
+        <div className="flex flex-row gap-3">
+          <button className="bg-orange-600 text-white font-semibold text-sm px-4 py-2 rounded-md hover:bg-orange-700 duration-150 transition-all cursor-pointer">
+            <Link href={"/pricing"}>Get API Key</Link>
+          </button>
+          <button className="hover:bg-stone-200 rounded-md p-1">
+            <Menu />
+          </button>
+        </div>
+      </div>
+    </header>
+  );
 }
