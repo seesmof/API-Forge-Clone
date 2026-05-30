5908b4873635fc12dbe6ec3f6642099dbec2d383
Oleg Onyshchenko <seesmwork@gmail.com>
2026-05-30 18:43:31 +0300

    feat: map over links, add cursor pointer to menu button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,18 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 
-const MenuItemClasses = "hover:text-stone-700 transition-all duration-150";
+interface Link {
+  title: string;
+  href: string;
+}
+
+const linksData: Link[] = [
+  { href: "/services", title: "Products" },
+  { href: "/pricing", title: "Pricing" },
+  { href: "/about", title: "About" },
+  { href: "/blog", title: "Blog" },
+  { href: "/contact", title: "Contact" },
+];
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
@@ -29,7 +40,7 @@ export default function Header() {
               <Link href={"/pricing"}>Get API Key</Link>
             </button>
             <button
-              className="hover:bg-stone-200 rounded-md p-1"
+              className="hover:bg-stone-200 rounded-md p-1 cursor-pointer"
               onClick={() => setIsMenuOpen((isMenuOpen) => !isMenuOpen)}
             >
               {isMenuOpen ? <X /> : <Menu />}
@@ -41,21 +52,15 @@ export default function Header() {
       {isMenuOpen && (
         <div className="bg-white border-b border-stone-200">
           <div className="font-semibold flex-col flex p-3 gap-5">
-            <Link className={MenuItemClasses} href={"/services"}>
-              Products
-            </Link>
-            <Link className={MenuItemClasses} href={"/pricing"}>
-              Pricing
-            </Link>
-            <Link className={MenuItemClasses} href={"/about"}>
-              About
-            </Link>
-            <Link className={MenuItemClasses} href={"/blog"}>
-              Blog
-            </Link>
-            <Link className={MenuItemClasses} href={"/contact"}>
-              Contact
-            </Link>
+            {linksData.map((link, index) => (
+              <Link
+                key={index}
+                href={link.href}
+                className="hover:text-stone-700 transition-all duration-150"
+              >
+                {link.title}
+              </Link>
+            ))}
           </div>
         </div>
       )}
