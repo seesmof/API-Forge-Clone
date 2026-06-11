3ae335dafe19449757d6fe8fd9e5075ec1dd5a68
Oleg Onyshchenko <seesmwork@gmail.com>
2026-06-11 19:28:38 +0300

    feat: make header and footer be persistent across pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 import ContactSection from "@/components/Contact";
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
 import HeroSection from "@/components/Hero";
 import PlatformSection from "@/components/Platform";
 import StartSection from "@/components/Start";
@@ -8,12 +6,10 @@ import StartSection from "@/components/Start";
 export default function Page() {
   return (
     <div className="flex flex-col">
-      <Header />
       <HeroSection />
       <PlatformSection />
       <StartSection />
       <ContactSection />
-      <Footer />
     </div>
   );
 }
