062dc3fad851ef078ed7b7222f4547fe7f3c3f6d
Oleg Onyshchenko <seesmwork@gmail.com>
2026-06-12 13:17:33 +0300

    feat: start working on the pricing section

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,3 +1,4 @@
+import Badge from "@/components/small/Badge";
 import Link from "next/link";
 
 export default function Page() {
@@ -20,6 +21,20 @@ export default function Page() {
             </Link>{" "}
             / Pricing
           </p>
+        </div>
+      </section>
+
+      {/* Pricing */}
+      <section>
+        <div className="px-4 py-12 mx-auto max-w-3xl flex flex-col">
+          <article className="text-center flex flex-col">
+            <Badge className="self-center">Pricing</Badge>
+            <h2 className="font-bold text-3xl my-4">Plans for Every Stage</h2>
+            <p>
+              Start with our generous free tier and scale seamlessly as your API
+              traffic grows.
+            </p>
+          </article>
         </div>
       </section>
     </>
